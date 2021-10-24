e3c35a1ba53dd8bbfb33b9ffb0903ebfe691cff2
SHAMS <shamsuddinpias0@gmail.com>
2021-10-24 15:29:13 +0600

    test

--- Cogs/lyrics.py
+++ Cogs/lyrics.py
@@ -3,10 +3,15 @@ from discord.ext import commands
 
 from Tools.Check import Check
 
+import aiohttp
+import datetime as dt
+
 
 class CogLyrics(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    LYRICS_URL = "https://some-random-api.ml/lyrics?title="
 
     @commands.command(name="lyrics",
                       aliases=["lyric", "Lyric"],
@@ -16,11 +21,33 @@ class CogLyrics(commands.Cog):
     @commands.cooldown(1, 5, commands.BucketType.member)
     async def lyrics(self, ctx):
 
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+
         if not await Check().userInVoiceChannel(ctx, self.bot): return
         if not await Check().botInVoiceChannel(ctx, self.bot): return
         if not await Check().userAndBotInSameVoiceChannel(ctx, self.bot): return
 
-        await ctx.send(f"{ctx.author.mention} Lyrics for Current music reload!")
+        name = player.current.title.replace("*", "\\*")
+
+        async with ctx.typing():
+            async with aiohttp.request("GET", self.LYRICS_URL + name, headers={}) as r:
+                if not 200 <= r.status <= 299:
+                    return await ctx.send("Lyrics Not Found!")
+
+                data = await r.json()
+
+                if len(data["lyrics"]) > 2000:
+                    return await ctx.send(f"<{data['links']['genius']}>")
+
+                embed = discord.Embed(
+                    title=data["title"],
+                    description=data["lyrics"],
+                    colour=ctx.author.colour,
+                    timestamp=dt.datetime.utcnow(),
+                )
+                embed.set_thumbnail(url=data["thumbnail"]["genius"])
+                embed.set_author(name=data["author"])
+                await ctx.send(embed=embed)
 
 
 def setup(bot):
--- Cogs/seek.py
+++ Cogs/seek.py
@@ -3,27 +3,42 @@ from discord.ext import commands
 
 from Tools.Check import Check
 
+import re
+
 
 class CogSeek(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="seek",
-                      aliases=["seek", "jump"],
+                      aliases=["seeks", "jump"],
                       usage="",
                       description="Seek the current music.")
     @commands.guild_only()
     @commands.cooldown(1, 5, commands.BucketType.member)
-    async def seek(self, ctx):
+    async def seek_new(self, ctx, position: str):
 
         if not await Check().userInVoiceChannel(ctx, self.bot): return
         if not await Check().botInVoiceChannel(ctx, self.bot): return
         if not await Check().userAndBotInSameVoiceChannel(ctx, self.bot): return
 
-        player = self.bot.wavelink.get_player(ctx.guild.id)
-        await player.seek(0)  # Reload the song
+        TIME_REGEX = r"([0-9]{1,2})[:ms](([0-9]{1,2})s?)?"
+        if not (match := re.match(TIME_REGEX, position)):
+            await ctx.send(f"{ctx.author.mention} Please Provide valid time. Example: 10s")
 
-        await ctx.send(f"{ctx.author.mention} Current music Seeked!")
+        if match.group(3):
+            secs = (int(match.group(1)) * 60) + (int(match.group(3)))
+        else:
+            secs = int(match.group(1))
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        await player.seek(secs * 1000)  # seek the song with second
+
+        position = divmod(player.position, 60000)
+        length = divmod(player.current.length, 60000)
+
+        await ctx.send(
+            f"{ctx.author.mention} Current music Seeked to {int(position[0])}:{round(position[1] / 1000):02}/{int(length[0])}:{round(length[1] / 1000):02}")
 
 
 def setup(bot):
